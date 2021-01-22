b09bcea0a47b0fcd1d007e0471174fa5d818d668
Nervflop <krystiannowak820@gmail.com>
2021-01-22 17:30:55 +0100

    bug fix

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './components/scss/App.scss'
+import './views/LandingPage.js'
+import LandingPage from './views/LandingPage.js'
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
